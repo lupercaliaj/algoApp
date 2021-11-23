e2ff47398215a373a3f064749a65325fee9d3976
lupercaliaj <nuoyizi2333@gmail.com>
2021-11-23 10:11:53 -0500

    initial commit

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static("pub"));
 
-app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>")
-})
+
 
 
 const port = process.env.PORT || 5000
